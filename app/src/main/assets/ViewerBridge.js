6a57cf299f38cc14ab7f0d396b9d306ea30a2242
Steve Chiu <steve.k.chiu@gmail.com>
2015-08-12 17:21:27 +0800

    Make it compatible with some Android device with enhanced security settings

--- app/src/main/assets/ViewerBridge.js
+++ app/src/main/assets/ViewerBridge.js
@@ -2,10 +2,34 @@
  * Android can not pass object and array, so we need to override it to convert
  * some of the function parameters to JSON
  */
-App = Object.create(_App);
+App = {};
+
+App.onChangeView = function(offset_x, offset_y, scale) {
+    _App.onChangeView(offset_x, offset_y, scale);
+};
+
+App.onChangeTitle = function(title) {
+    _App.onChangeTitle(title);
+}
 
 App.onChangeTOC = function(toc_entry_array) {
     _App.onChangeTOC(JSON.stringify(toc_entry_array));
+};
+
+App.onChangePage = function(chapter, cfi, current_page, total_pages) {
+    _App.onChangePage(chapter, cfi, current_page, total_pages);
+};
+
+App.onTrackAction = function(action, cfi) {
+    _App.onTrackAction(action, cfi);
+};
+
+App.onToggleToolbar = function(visible) {
+    _App.onToggleToolbar(visible);
+};
+
+App.onRequestHighlights = function(chapter, callback) {
+    _App.onRequestHighlights(chapter, callback);
 };
 
 App.onAddHighlight = function(chapter, highlight, callback) {
@@ -16,12 +40,32 @@ App.onUpdateHighlight = function(highlight) {
     _App.onUpdateHighlight(JSON.stringify(highlight));
 };
 
+App.onRemoveHighlight = function(uuid) {
+    _App.onRemoveHighlight(uuid);
+};
+
+App.onShareHighlight = function(uuid) {
+    _App.onShareHighlight(uuid);
+};
+
+App.onAnnotateHighlight = function(uuid) {
+    _App.onAnnotateHighlight(uuid);
+};
+
+App.onRequestBookmarks = function(chapter, callback) {
+    _App.onRequestBookmarks(chapter, callback);
+};
+
 App.onAddBookmark = function(chapter, bookmark, callback) {
     _App.onAddBookmark(chapter, JSON.stringify(bookmark), callback);
 };
 
 App.onUpdateBookmark = function(bookmark) {
     _App.onUpdateBookmark(JSON.stringify(bookmark));
+};
+
+App.onRemoveBookmark = function((uuid) {
+    _App.onRemoveBookmark(uuid);
 };
 
 App.onSearchResult = function(keyword, result_array) {
